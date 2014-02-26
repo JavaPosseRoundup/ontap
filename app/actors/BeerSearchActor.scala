f2d33e2a6006efdcfd4daf2c3a2261c68a7478b3
James Ward <james@jamesward.org>
2014-02-26 10:29:36 -0700

    add beer search stub

--- app/actors/BeerSearchActor.scala
+++ app/actors/BeerSearchActor.scala
@@ -1,11 +1,24 @@
 package actors
 
 import akka.actor.Actor
+import play.api.libs.json.{Json, Format}
 
 class BeerSearchActor extends Actor {
 
   def receive = {
-    case _ => context.system.log.info("hello")
+    case doSearch: DoSearch =>
+      context.system.log.info(doSearch.toString)
+      sender ! SearchResults("asdf")
+    case _ =>
+      context.system.log.info("hello")
   }
   
+}
+
+case class DoSearch(query: String, location: String)
+
+case class SearchResults(foo: String)
+
+object SearchResults {
+  implicit val format: Format[SearchResults] = Json.format[SearchResults]
 }
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,18 +3,40 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.concurrent.Akka
-import actors.BeerSearchActor
-import akka.actor.Props
+import actors.{SearchResults, DoSearch, BeerSearchActor}
+import akka.pattern.ask
+import akka.actor.{ActorRef, Props}
 import play.api.Play.current
+import scala.concurrent.Future
+import play.api.libs.json.Json
+import akka.util.Timeout
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 object Application extends Controller {
 
+  implicit val askTimeout = Timeout(15.seconds)
+  
   def index = Action {
-    val beerSearchActorRef = Akka.system.actorOf(Props[BeerSearchActor])
-    
-    beerSearchActorRef ! "a message"
-    
     Ok(views.html.index("Your new application is ready."))
+  }
+  
+  def doSearch(query: String, location: String) = Action.async {
+    val beerSearchActorRef: ActorRef = Akka.system.actorOf(Props[BeerSearchActor])
+
+    val searchResults: Future[Any] = beerSearchActorRef ? DoSearch(query, location)
+    
+    val resultFuture: Future[SimpleResult] = searchResults.map {
+      case searchResults: SearchResults =>
+        Ok(Json.toJson(searchResults))
+      case _ =>
+        InternalServerError("Unknown Error")
+    } recover {
+      case e: Exception =>
+        InternalServerError(e.toString)
+    }
+    
+    resultFuture
   }
 
 }
