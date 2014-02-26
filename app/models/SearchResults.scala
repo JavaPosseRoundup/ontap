09552c414e6493b07af3735f96f50aa59d50d1b4
Dick Wall <dwall@bldc.org>
2014-02-26 10:26:41 -0800

    Added XML format for SearchResults and test.

--- app/models/SearchResults.scala
+++ app/models/SearchResults.scala
@@ -1,9 +1,44 @@
 package models
 
 import play.api.libs.json.{Json, Format}
+import models.xmlutil.XmlFormat
+import scala.xml.Node
 
 case class SearchResult(id: String, name: String, status: String, street: String, city: String, state: String, zip: String, country: String)
 
 object SearchResult {
-  implicit val format: Format[SearchResult] = Json.format[SearchResult]
+  implicit val jsonFormat: Format[SearchResult] = Json.format[SearchResult]
+
+  implicit val xmlFormat = new XmlFormat[SearchResult] {
+    override def writeXml(item: SearchResult): Node = {
+      <location>
+        <id>{item.id}</id>
+        <name>{item.name}</name>
+        <status>{item.status}</status>
+        <reviewlink></reviewlink>
+        <proxylink></proxylink>
+        <blogmap></blogmap>
+        <street>{item.street}</street>
+        <city>{item.city}</city>
+        <state>{item.state}</state>
+        <zip>{item.zip}</zip>
+        <country>{item.country}</country>
+        <phone></phone>
+        <overall></overall>
+        <imagecount></imagecount>
+      </location>
+    }
+
+    override def readXml(node: Node): SearchResult = {
+      val id = (node \ "id").text
+      val name = (node \ "name").text
+      val status = (node \ "status").text
+      val street = (node \ "street").text
+      val city = (node \ "city").text
+      val state = (node \ "state").text
+      val zip = (node \ "zip").text
+      val country = (node \ "country").text
+      SearchResult(id, name, status, street, city, state, zip, country)
+    }
+  }
 }
--- build.sbt
+++ build.sbt
@@ -4,7 +4,8 @@ version := "1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
   "org.reactivemongo" %% "play2-reactivemongo" % "0.10.2",
-  "org.webjars" %% "webjars-play" % "2.2.1-2"
+  "org.webjars" %% "webjars-play" % "2.2.1-2",
+  "org.scalatest" %% "scalatest" % "2.0" % "test"
 )
 
 play.Project.playScalaSettings
