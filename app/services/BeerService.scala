e4dace808002e014f363b3c580283aee4d051242
Matt Zimmer <mzimmer@stumbleupon.com>
2014-02-26 18:14:33 -0700

    Integrate with the pub service; Return beers for a pub.
    Added PubId as a sealed trait for typed pubs.

--- app/services/BeerService.scala
+++ app/services/BeerService.scala
@@ -1,19 +1,25 @@
 package services
 
-import models.{Pub, PubSeq}
+import models._
 import scala.concurrent.Future
-import play.api.{Logger, Play}
-import play.api.libs.json.Json
-import play.api.Play.current
-import com.github.davidmoten.geo.GeoHash
+import models.Beer
 
 trait BeerService {
-  
-  
-
+  def beersForPub(pubId: PubId): Future[Seq[Beer]]
 }
 
 class FakeBeerService extends BeerService {
-  
-  
+  override def beersForPub(pubId: PubId): Future[Seq[Beer]] =
+    pubId match {
+      case DiveBar =>
+        Future.successful(Seq.empty)
+      case BrickOvenPizza =>
+        val pliny = Beer(
+          "Pliny",
+          "Russian River Brewery",
+          "Double IPA",
+          Ale
+        )
+        Future.successful(Seq(pliny))
+    }
 }
